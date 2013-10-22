060d2838c20f2185145b8415142c95da29d00162
new account <alejandr@paris.ins.cwi.nl>
2013-10-22 16:52:22 +0200

    First commit

---
+++ src/main/java/net/recommenders/evaluation/splitter/RandomSplitter.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.recommenders.evaluation.splitter;
+
+/**
+ *
+ * @author alejandr
+ */
+public class RandomSplitter {
+    
+}
---
+++ src/main/java/net/recommenders/evaluation/splitter/data/Movielens100kParser.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.recommenders.evaluation.splitter.data;
+
+import java.io.File;
+import net.recommenders.evaluation.core.DataModel;
+
+/**
+ *
+ * @author alejandr
+ */
+public class Movielens100kParser {
+    
+    public DataModel<Long, Long> parseData(File f){
+        return null;
+    }
+}
