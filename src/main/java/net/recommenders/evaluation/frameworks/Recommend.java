5914e54f2cc9381907e32341b0f5ca88b5ce380c
new account <alejandr@paris.ins.cwi.nl>
2013-10-22 16:50:22 +0200

    First commit

---
+++ src/main/java/net/recommenders/evaluation/frameworks/Recommend.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.recommenders.evaluation.frameworks;
+
+/**
+ *
+ * @author alejandr
+ */
+public class Recommend {
+    public static void main(String[] args) {
+        
+    }
+}
