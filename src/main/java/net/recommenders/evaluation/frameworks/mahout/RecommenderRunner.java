1a7469aa3291f434f3b8c6658f473b09fd465067
Alan Said <alan@alans.se>
2013-11-22 12:40:12 +0100

    user-based, item-based and SVD recommenders done

--- src/main/java/net/recommenders/evaluation/frameworks/mahout/RecommenderRunner.java
+++ src/main/java/net/recommenders/evaluation/frameworks/mahout/RecommenderRunner.java
@@ -4,36 +4,56 @@
  */
 package net.recommenders.evaluation.frameworks.mahout;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
+import net.recommenders.evaluation.frameworks.Recommend;
+import net.recommenders.evaluation.frameworks.mahout.exceptions.AbstractRunner;
+import net.recommenders.evaluation.frameworks.mahout.exceptions.RecommenderException;
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.Recommender;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
- * @author alejandr
+ * @author alejandr, alan
  */
-public class RecommenderRunner {
+public class RecommenderRunner extends AbstractRunner{
 
-    private String inputPath;
-    private String outputPath;
+    public static final int DEFAULT_NEIGHBORHOOD_SIZE = 50;
     private Map<String, String> parameters;
-
-    public void run() throws IOException, TasteException {
+    public void runRecommender() throws IOException, TasteException {
+        DataModel trainModel = new FileDataModel(new File(parameters.get(Recommend.trainingSet)));
+        DataModel testModel = new FileDataModel(new File(parameters.get(Recommend.testSet)));
+        GenericRecommenderBuilder grb = new GenericRecommenderBuilder();
         Recommender recommender = null;
-        DataModel model = null;
-        // instantiate everything
-        model = new FileDataModel(new File(inputPath));
-        // 
-        LongPrimitiveIterator users = model.getUserIDs();
+        try{
+            if(parameters.get(Recommend.factors) == null)
+                recommender = grb.buildRecommender(trainModel, parameters.get(Recommend.recommender), parameters.get(Recommend.similarity), parameters.get(Recommend.neighborhood));
+            if(parameters.get(Recommend.factors) != null)
+                recommender = grb.buildRecommender(trainModel, parameters.get(Recommend.recommender), parameters.get(Recommend.factorizer), Integer.parseInt(parameters.get(Recommend.iterations)), Integer.parseInt(parameters.get(Recommend.factors)));
+            System.out.println(recommender.getClass().getName());
+        }catch (RecommenderException e){
+            e.printStackTrace();
+        }
+
+        LongPrimitiveIterator users = testModel.getUserIDs();
         while (users.hasNext()) {
             long u = users.nextLong();
-            recommender.recommend(u, model.getNumItems());
-            // write to disk
+            List<RecommendedItem> items = recommender.recommend(u, trainModel.getNumItems());
+            writeData(parameters.get(Recommend.output), u, items);
         }
     }
+
+
+    public void setParameters(Map<String, String> parameters) {
+        this.parameters = parameters;
+    }
+
+
 }
