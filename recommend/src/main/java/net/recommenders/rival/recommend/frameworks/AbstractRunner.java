bc40a2bf3e5cca64498bf7a755e1abc9efbdf4e4
Alejandro Bellogin <alejandro.bellogin@uam.es>
2013-12-05 17:43:36 +0100

    Style issues in Recommend/RR

--- recommend/src/main/java/net/recommenders/rival/recommend/frameworks/AbstractRunner.java
+++ recommend/src/main/java/net/recommenders/rival/recommend/frameworks/AbstractRunner.java
@@ -12,44 +12,40 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 
-/**
- * Created with IntelliJ IDEA.
- * User: alan
- * Date: 2013-11-22
- * Time: 11:34
- */
-public class AbstractRunner {
+public abstract class AbstractRunner {
+
     private final static Logger logger = LoggerFactory.getLogger(AbstractRunner.class);
     public static final int DEFAULT_ITERATIONS = 50;
     public Properties properties;
     private String fileName;
     private String path;
-    public boolean alreadyRecomended = false;
+    protected boolean alreadyRecomended;
 
-    public AbstractRunner(Properties properties){
+    public AbstractRunner(Properties properties) {
         this.properties = properties;
         this.setFileName();
         String filePath = properties.getProperty(RecommendationRunner.output) + "/" + fileName;
-        File file = new File(filePath);
-        if (file.exists()){
+        alreadyRecomended = new File(filePath).exists();
+        if (alreadyRecomended) {
             System.out.println("File exists: " + filePath);
-            alreadyRecomended = true;
         }
         path = properties.getProperty(RecommendationRunner.output);
-
     }
 
     public <T> void writeData(long user, List<T> recommendations) {
         try {
             File dir = new File(path);
-            if (!dir.isDirectory())
+            if (!dir.isDirectory()) {
                 dir.mkdir();
+            }
             BufferedWriter out = new BufferedWriter(new FileWriter(path + "/" + fileName, true));
-            for(Object ri : recommendations){
-                if (ri instanceof RecommendedItem)
-                    out.write(user + "\t" + ((RecommendedItem)ri).getItemID() + "\t" + ((RecommendedItem)ri).getValue() + "\n");
-                if (ri instanceof ScoredId)
-                    out.write(user + "\t" + ((ScoredId)ri).getId() + "\t" + ((ScoredId)ri).getScore() + "\n");
+            for (Object ri : recommendations) {
+                if (ri instanceof RecommendedItem) {
+                    out.write(user + "\t" + ((RecommendedItem) ri).getItemID() + "\t" + ((RecommendedItem) ri).getValue() + "\n");
+                }
+                if (ri instanceof ScoredId) {
+                    out.write(user + "\t" + ((ScoredId) ri).getId() + "\t" + ((ScoredId) ri).getScore() + "\n");
+                }
             }
             out.flush();
             out.close();
@@ -59,35 +55,36 @@ public class AbstractRunner {
         }
     }
 
-    public void setFileName(){
+    public void setFileName() {
         String type = "";
         // lenskit does not provide a factorizer class. This check is to actually see if it's a Mahout or Lenskit SVD.
-        if(properties.containsKey(RecommendationRunner.factorizer) || properties.containsKey(RecommendationRunner.similarity)){
-            type = (properties.containsKey(RecommendationRunner.factorizer) ?
-                    properties.getProperty(RecommendationRunner.factorizer) :
-                    properties.getProperty(RecommendationRunner.similarity));
-            type = type.substring(type.lastIndexOf(".") + 1)+".";
+        if (properties.containsKey(RecommendationRunner.factorizer) || properties.containsKey(RecommendationRunner.similarity)) {
+            type = (properties.containsKey(RecommendationRunner.factorizer)
+                    ? properties.getProperty(RecommendationRunner.factorizer)
+                    : properties.getProperty(RecommendationRunner.similarity));
+            type = type.substring(type.lastIndexOf(".") + 1) + ".";
         }
         String num = "";
-        if (properties.containsKey(RecommendationRunner.factors) || properties.containsKey(RecommendationRunner.neighborhood))
-            num = (properties.containsKey(RecommendationRunner.factors) ?
-                    properties.getProperty(RecommendationRunner.factors) :
-                    properties.getProperty(RecommendationRunner.neighborhood)) + ".";
+        if (properties.containsKey(RecommendationRunner.factors) || properties.containsKey(RecommendationRunner.neighborhood)) {
+            num = (properties.containsKey(RecommendationRunner.factors)
+                    ? properties.getProperty(RecommendationRunner.factors)
+                    : properties.getProperty(RecommendationRunner.neighborhood)) + ".";
+        }
 
-        String trainingSet =  properties.getProperty(RecommendationRunner.trainingSet);
+        String trainingSet = properties.getProperty(RecommendationRunner.trainingSet);
         trainingSet = trainingSet.substring(trainingSet.lastIndexOf("/") + 1, trainingSet.lastIndexOf("."));
 
-        fileName = trainingSet + "." +
-                properties.getProperty(RecommendationRunner.framework) + "." +
-                properties.getProperty(RecommendationRunner.recommender).substring(properties.getProperty(RecommendationRunner.recommender).lastIndexOf(".") + 1) + "." +
-                type +
-                num +
-                "tsv";
+        fileName = trainingSet + "."
+                + properties.getProperty(RecommendationRunner.framework) + "."
+                + properties.getProperty(RecommendationRunner.recommender).substring(properties.getProperty(RecommendationRunner.recommender).lastIndexOf(".") + 1) + "."
+                + type
+                + num
+                + "tsv";
 
         System.out.println(fileName);
     }
 
-    public String getCanonicalFileName(){
+    public String getCanonicalFileName() {
         return path + "/" + fileName + ".stats";
     }
 
@@ -95,11 +92,9 @@ public class AbstractRunner {
         this.properties = properties;
     }
 
-    public boolean getAlreadyRecommended(){
+    public boolean getAlreadyRecommended() {
         return alreadyRecomended;
     }
 
-    public void run() throws Exception{
-
-    }
+    public abstract void run() throws Exception;
 }
--- recommend/src/main/java/net/recommenders/rival/recommend/frameworks/RecommendationRunner.java
+++ recommend/src/main/java/net/recommenders/rival/recommend/frameworks/RecommendationRunner.java
@@ -4,19 +4,22 @@
  */
 package net.recommenders.rival.recommend.frameworks;
 
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import net.recommenders.rival.recommend.frameworks.lenskit.LenskitRecommenderRunner;
 import net.recommenders.rival.recommend.frameworks.mahout.MahoutRecommenderRunner;
 
-import java.io.*;
 import java.util.Properties;
 
 /**
  *
  * @author alejandr
  */
-
-
 public class RecommendationRunner {
+
     public static final String recommender = "recommender";
     public static final String similarity = "similarity";
     public static final String factorizer = "factorizer";
@@ -32,78 +35,66 @@ public class RecommendationRunner {
     public static String statPath;
     public static long time;
 
-
     public static void main(String[] args) {
         String propertyFile = System.getProperty("file");
-        if(propertyFile == null){
+        if (propertyFile == null) {
             System.out.println("Property file not given, exiting.");
             System.exit(0);
         }
         final Properties properties = new Properties();
-        try{
+        try {
             properties.load(new FileInputStream(propertyFile));
-        } catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException ie){
+        } catch (IOException ie) {
             ie.printStackTrace();
         }
         recommend(properties);
     }
 
-    public static void recommend(Properties properties){
-        if(properties.getProperty(recommender) == null){
+    public static void recommend(Properties properties) {
+        if (properties.getProperty(recommender) == null) {
             System.out.println("No recommenderClass specified, exiting.");
             return;
         }
-        if (properties.getProperty(trainingSet) == null){
+        if (properties.getProperty(trainingSet) == null) {
             System.out.println("No training set specified, exiting.");
             return;
         }
-        if (properties.getProperty(testSet) == null){
+        if (properties.getProperty(testSet) == null) {
             System.out.println("No training set specified, exiting.");
             return;
         }
         time = System.currentTimeMillis();
 
-        AbstractRunner rr;
+        AbstractRunner rr = null;
         boolean statsExist = false;
-        if (properties.getProperty(framework).equals(MAHOUT)){
+        if (properties.getProperty(framework).equals(MAHOUT)) {
             rr = new MahoutRecommenderRunner(properties);
-            statPath = rr.getCanonicalFileName();
-            statsExist = rr.getAlreadyRecommended();
-            try{
-                rr.run();
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-        else if (properties.getProperty(framework).equals(LENSKIT)){
+        } else if (properties.getProperty(framework).equals(LENSKIT)) {
             rr = new LenskitRecommenderRunner(properties);
-            statPath = rr.getCanonicalFileName();
-            statsExist = rr.getAlreadyRecommended();
-            try {
-                rr.run();
-            } catch (Exception e){
-                e.printStackTrace();
-            }
+        }
+        statPath = rr.getCanonicalFileName();
+        statsExist = rr.getAlreadyRecommended();
+        try {
+            rr.run();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         time = System.currentTimeMillis() - time;
-        if (!statsExist){
+        if (!statsExist) {
             writeStats(statPath, "time", time);
         }
     }
 
-    public static void writeStats(String path, String statLabel, long stat){
+    public static void writeStats(String path, String statLabel, long stat) {
         try {
             BufferedWriter out = new BufferedWriter(new FileWriter(path, true));
             out.write(statLabel + "\t" + stat + "\n");
             out.flush();
             out.close();
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            e.printStackTrace();
         }
     }
-
-
-
 }
