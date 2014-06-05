97fec38347f8c9ee57dedafd99eb25922e9ea6c6
Alan Said <alan@alans.se>
2014-06-05 02:19:01 +0200

    Example module with empty skeleton class

---
+++ rival-examples/src/main/java/net/recommenders/rival/examples/CrossValidatedMahoutKNNRecommenderWithThreshold.java
@@ -0,0 +1,8 @@
+package net.recommenders.rival.examples;
+/**
+ *
+ *
+ * @author <a href="http://github.com/alansaid">Alan</a>
+ */
+public class CrossValidatedMahoutKNNRecommenderWithThreshold {
+}
