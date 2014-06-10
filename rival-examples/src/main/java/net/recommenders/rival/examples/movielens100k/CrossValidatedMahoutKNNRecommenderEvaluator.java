82e86aed8027d69d43c7c83f941870a7235535c2
Alan Said <alan@alans.se>
2014-06-10 18:24:17 +0200

    First example done. See #54

--- rival-examples/src/main/java/net/recommenders/rival/examples/movielens100k/CrossValidatedMahoutKNNRecommenderEvaluator.java
+++ rival-examples/src/main/java/net/recommenders/rival/examples/movielens100k/CrossValidatedMahoutKNNRecommenderEvaluator.java
@@ -3,6 +3,10 @@ package net.recommenders.rival.examples.movielens100k;
 import net.recommenders.rival.core.DataModel;
 import net.recommenders.rival.core.Parser;
 import net.recommenders.rival.core.SimpleParser;
+import net.recommenders.rival.evaluation.metric.error.RMSE;
+import net.recommenders.rival.evaluation.metric.ranking.MAP;
+import net.recommenders.rival.evaluation.metric.ranking.NDCG;
+import net.recommenders.rival.evaluation.metric.ranking.Precision;
 import net.recommenders.rival.evaluation.strategy.EvaluationStrategy;
 import net.recommenders.rival.recommend.frameworks.RecommenderIO;
 import net.recommenders.rival.recommend.frameworks.mahout.GenericRecommenderBuilder;
@@ -34,7 +38,85 @@ public class CrossValidatedMahoutKNNRecommenderEvaluator {
         prepareSplits(url, nFolds);
         recommend(nFolds);
         prepareStrategy(nFolds);
-//        evaluate();
+        evaluate(nFolds);
+    }
+
+    public static void prepareSplits(String url, int nFolds) {
+        DataDownloader dd = new DataDownloader(url);
+        dd.downloadAndUnzip();
+
+        boolean perUser = true;
+        long seed = 2048;
+        Parser parser = new MovielensParser();
+
+        DataModel<Long, Long> data = null;
+        try {
+            data = parser.parseData(new File("data/ml-100k/u.data"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DataModel<Long, Long>[] splits = new CrossValidationSplitter(nFolds, perUser, seed).split(data);
+        String path = "data/model/";
+        File dir = new File(path);
+        if (!dir.exists())
+            dir.mkdir();
+        for (int i = 0; i < splits.length / 2; i++) {
+            DataModel<Long, Long> training = splits[2 * i];
+            DataModel<Long, Long> test = splits[2 * i + 1];
+            String trainingFile = path + "train." + i + ".csv";
+            String testFile = path + "test." + i + ".csv";
+            System.out.println("train: " + trainingFile);
+            System.out.println("test: " + testFile);
+            boolean overwrite = true;
+            try {
+                training.saveDataModel(trainingFile, overwrite);
+                test.saveDataModel(testFile, overwrite);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void recommend(int nFolds) {
+        for (int i = 0; i < nFolds; i++) {
+            org.apache.mahout.cf.taste.model.DataModel trainModel = null;
+            org.apache.mahout.cf.taste.model.DataModel testModel = null;
+            try {
+                trainModel = new FileDataModel(new File("data/model/train." + i + ".csv"));
+                testModel = new FileDataModel(new File("data/model/test." + i + ".csv"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            GenericRecommenderBuilder grb = new GenericRecommenderBuilder();
+            String recommenderClass = "org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender";
+            String similarityClass = "org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity";
+            int neighborhoodSize = 50;
+            Recommender recommender = null;
+            try {
+                recommender = grb.buildRecommender(trainModel, recommenderClass, similarityClass, neighborhoodSize);
+            } catch (TasteException e) {
+                e.printStackTrace();
+            } catch (RecommenderException e) {
+                e.printStackTrace();
+            }
+
+            String path = "data/recommendations";
+            String fileName = "recs." + i + ".csv";
+
+            LongPrimitiveIterator users = null;
+            try {
+                users = testModel.getUserIDs();
+                while (users.hasNext()) {
+                    long u = users.nextLong();
+                    List<RecommendedItem> items = recommender.recommend(u, trainModel.getNumItems());
+                    RecommenderIO.writeData(u, items, path, fileName);
+                }
+            } catch (TasteException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void prepareStrategy(int nFolds) {
@@ -75,117 +157,42 @@ public class CrossValidatedMahoutKNNRecommenderEvaluator {
 
             DataModel<Long, Long> modelToEval = new DataModel<Long, Long>();
 
-            //System.out.println("recusers: " + recModel.getNumUsers());
-            // no matches...
             for (Long user : recModel.getUsers())
                 for (Long item : strategy.getCandidateItemsToRank(user))
-                    if (recModel.getUserItemPreferences().get(user).containsValue(item))
+                    if (recModel.getUserItemPreferences().get(user).containsKey(item)) {
                         modelToEval.addPreference(user, item, recModel.getUserItemPreferences().get(user).get(item));
+                    }
             try {
                 modelToEval.saveDataModel("data/model/strategymodel." + i + ".csv", true);
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
         }
-        /**
-         final Map<Long, List<EvaluationStrategy.Pair<Long, Double>>> mapUserRecommendations = new HashMap<Long, List<EvaluationStrategy.Pair<Long, Double>>>();
-         BufferedReader in = null;
-         try {
-         in = new BufferedReader(new FileReader(recFile));
-         } catch (FileNotFoundException e) {
-         e.printStackTrace();
-         }
-         String line = null;
-         try {
-         while ((line = in.readLine()) != null) {
-         StrategyIO.readLine(line, mapUserRecommendations);
-         }
-         in.close();
-         } catch (IOException e) {
-         e.printStackTrace();
-         }
-         */
     }
 
-
-    public static void recommend(int nFolds) {
+    public static void evaluate(int nFolds){
+        double ndcgRes = 0.0;
+        double precisionRes = 0.0;
         for (int i = 0; i < nFolds; i++) {
-            org.apache.mahout.cf.taste.model.DataModel trainModel = null;
-            org.apache.mahout.cf.taste.model.DataModel testModel = null;
+            File testFile = new File("data/model/test." + i + ".csv");
+            File recFile = new File("data/recommendations/recs." + i + ".csv");
+            DataModel<Long, Long> testModel = null;
+            DataModel<Long, Long> recModel = null;
             try {
-                trainModel = new FileDataModel(new File("data/model/train." + i + ".csv"));
-                testModel = new FileDataModel(new File("data/model/test." + i + ".csv"));
+                testModel = new SimpleParser().parseData(testFile);
+                recModel = new SimpleParser().parseData(recFile);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            NDCG ndcg = new NDCG(recModel, testModel, new int[]{10});
+            ndcg.compute();
+            ndcgRes += ndcg.getValueAt(10);
+//            System.out.println("NDCG@10: " + ndcg.getValueAt(10));
 
-            GenericRecommenderBuilder grb = new GenericRecommenderBuilder();
-            String recommenderClass = "org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender";
-            String similarityClass = "org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity";
-            int neighborhoodSize = 50;
-            Recommender recommender = null;
-            try {
-                recommender = grb.buildRecommender(trainModel, recommenderClass, similarityClass, neighborhoodSize);
-            } catch (TasteException e) {
-                e.printStackTrace();
-            } catch (RecommenderException e) {
-                e.printStackTrace();
-            }
-
-            String path = "data/recommendations";
-            String fileName = "recs." + i + ".csv";
-
-            LongPrimitiveIterator users = null;
-            try {
-                users = testModel.getUserIDs();
-                while (users.hasNext()) {
-                    long u = users.nextLong();
-                    List<RecommendedItem> items = recommender.recommend(u, trainModel.getNumItems());
-                    RecommenderIO.writeData(u, items, path, fileName);
-                }
-            } catch (TasteException e) {
-                e.printStackTrace();
-            }
-
+//            RMSE rmse = new RMSE(recModel, testModel);
+//            rmse.compute();
+//            System.out.println(rmse.getValue());
         }
+        System.out.println("NDCG@10: " + ndcgRes/nFolds);
     }
-
-    public static void prepareSplits(String url, int nFolds) {
-        DataDownloader dd = new DataDownloader(url);
-        dd.downloadAndUnzip();
-
-        boolean perUser = true;
-        long seed = 2048;
-        Parser parser = new MovielensParser();
-
-        DataModel<Long, Long> data = null;
-        try {
-            data = parser.parseData(new File("data/ml-100k/u.data"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        DataModel<Long, Long>[] splits = new CrossValidationSplitter(nFolds, perUser, seed).split(data);
-        String path = "data/model/";
-        File dir = new File(path);
-        if (!dir.exists())
-            dir.mkdir();
-        for (int i = 0; i < splits.length / 2; i++) {
-            DataModel<Long, Long> training = splits[2 * i];
-            DataModel<Long, Long> test = splits[2 * i + 1];
-            String trainingFile = path + "train." + i + ".csv";
-            String testFile = path + "test." + i + ".csv";
-            System.out.println("train: " + trainingFile);
-            System.out.println("test: " + testFile);
-            boolean overwrite = true;
-            try {
-                training.saveDataModel(trainingFile, overwrite);
-                test.saveDataModel(testFile, overwrite);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
 }
