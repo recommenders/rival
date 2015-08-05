8e1c8e652aae9fdcada49fcbfb867ae093c6ef10
Alejandro <alejandro.bellogin@uam.es>
2015-08-05 16:02:35 +0200

    Improved documentation (issue #86)
    Added license to all classes

--- rival-examples/src/main/java/net/recommenders/rival/examples/movietweetings/RandomMahoutIBRecommenderEvaluator.java
+++ rival-examples/src/main/java/net/recommenders/rival/examples/movietweetings/RandomMahoutIBRecommenderEvaluator.java
@@ -1,6 +1,30 @@
+/*
+ * Copyright 2015 recommenders.net.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.recommenders.rival.examples.movietweetings;
 
-import net.recommenders.rival.core.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import net.recommenders.rival.core.DataModel;
+import net.recommenders.rival.core.DataModelUtils;
+import net.recommenders.rival.core.SimpleParser;
+import net.recommenders.rival.core.UIPParser;
 import net.recommenders.rival.evaluation.metric.error.RMSE;
 import net.recommenders.rival.evaluation.metric.ranking.NDCG;
 import net.recommenders.rival.evaluation.metric.ranking.Precision;
@@ -15,13 +39,6 @@ import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.Recommender;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
 
 /**
  * Example where data from MovieTweetings is used to evaluate an IB recommender.
