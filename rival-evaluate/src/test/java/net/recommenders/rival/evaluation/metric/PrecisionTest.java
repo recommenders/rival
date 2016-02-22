78f36445a8df3c8dd52d88f2671a206c273b0665
Alejandro <alejandro.bellogin@uam.es>
2016-02-22 12:56:28 +0100

    Test for issue #113

--- rival-evaluate/src/test/java/net/recommenders/rival/evaluation/metric/PrecisionTest.java
+++ rival-evaluate/src/test/java/net/recommenders/rival/evaluation/metric/PrecisionTest.java
@@ -22,6 +22,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import java.util.Map;
+import net.recommenders.rival.evaluation.metric.ranking.Recall;
 
 import static org.junit.Assert.assertEquals;
 
@@ -149,5 +150,65 @@ public class PrecisionTest {
             double value = e.getValue();
             assertEquals(0.5, value, 0.001);
         }
+    }
+
+    @Test
+    public void testIssue113() {
+        boolean useBinaryScores = false; // if true, first configuration, otherwise, second configuration
+        DataModel<Long, Long> predictions = new DataModel<Long, Long>();
+        DataModel<Long, Long> test = new DataModel<Long, Long>();
+
+        predictions.addPreference(2L, 13L, useBinaryScores ? 1.0 : 0.5328112138740229);
+        test.addPreference(2L, 13L, 1.0);
+        predictions.addPreference(2L, 19L, useBinaryScores ? 1.0 : 0.8095414650648188);
+        test.addPreference(2L, 19L, 0.0);
+        predictions.addPreference(2L, 50L, useBinaryScores ? 1.0 : 0.9888651730555137);
+        test.addPreference(2L, 50L, 1.0);
+        predictions.addPreference(2L, 251L, useBinaryScores ? 1.0 : 0.8796398906715018);
+        test.addPreference(2L, 251L, 1.0);
+        predictions.addPreference(2L, 257L, useBinaryScores ? 1.0 : 0.8994154288885712);
+        test.addPreference(2L, 257L, 1.0);
+        predictions.addPreference(2L, 279L, useBinaryScores ? 0.0 : 0.2031462463186414);
+        test.addPreference(2L, 279L, 1.0);
+        predictions.addPreference(2L, 280L, useBinaryScores ? 0.0 : 0.137729028181314);
+        test.addPreference(2L, 280L, 0.0);
+        predictions.addPreference(2L, 281L, useBinaryScores ? 0.0 : 0.12677275323108753);
+        test.addPreference(2L, 281L, 0.0);
+        predictions.addPreference(2L, 290L, useBinaryScores ? 0.0 : 0.12267777345041282);
+        test.addPreference(2L, 290L, 0.0);
+        predictions.addPreference(2L, 292L, useBinaryScores ? 1.0 : 0.6708907647085832);
+        test.addPreference(2L, 292L, 1.0);
+        predictions.addPreference(2L, 297L, useBinaryScores ? 1.0 : 0.769468001849272);
+        test.addPreference(2L, 297L, 1.0);
+        predictions.addPreference(2L, 298L, useBinaryScores ? 1.0 : 0.878495414483143);
+        test.addPreference(2L, 298L, 0.0);
+        predictions.addPreference(2L, 299L, useBinaryScores ? 0.0 : 0.09460751859396674);
+        test.addPreference(2L, 299L, 1.0);
+        predictions.addPreference(2L, 301L, useBinaryScores ? 1.0 : 0.5983346178722854);
+        test.addPreference(2L, 301L, 1.0);
+        predictions.addPreference(2L, 303L, useBinaryScores ? 1.0 : 0.8094825203837621);
+        test.addPreference(2L, 303L, 1.0);
+        predictions.addPreference(2L, 307L, useBinaryScores ? 1.0 : 0.7223299632360657);
+        test.addPreference(2L, 307L, 0.0);
+        predictions.addPreference(2L, 308L, useBinaryScores ? 0.0 : 0.11311986767160687);
+        test.addPreference(2L, 308L, 0.0);
+        predictions.addPreference(2L, 312L, useBinaryScores ? 0.0 : 0.1279286169284483);
+        test.addPreference(2L, 312L, 0.0);
+        predictions.addPreference(2L, 313L, useBinaryScores ? 1.0 : 0.9889272842775845);
+        test.addPreference(2L, 313L, 1.0);
+        predictions.addPreference(2L, 314L, useBinaryScores ? 0.0 : 0.0026441894503800604);
+        test.addPreference(2L, 314L, 0.0);
+        predictions.addPreference(2L, 315L, useBinaryScores ? 1.0 : 0.955807550363639);
+        test.addPreference(2L, 315L, 0.0);
+        predictions.addPreference(2L, 316L, useBinaryScores ? 1.0 : 0.965191057772059);
+        test.addPreference(2L, 316L, 1.0);
+
+        Precision<Long, Long> precision = new Precision<Long, Long>(predictions, test);
+        precision.compute();
+        assertEquals(0.545, precision.getValue(), 0.001);
+
+        Recall<Long, Long> recall = new Recall<Long, Long>(predictions, test);
+        recall.compute();
+        assertEquals(1.0, recall.getValue(), 0.);
     }
 }
